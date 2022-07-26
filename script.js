8c7aac9343ddcc5faa5232fbce397f200bead737
Nick <Nick_II2000@mail.ru>
2022-07-26 08:56:47 +0600

    6-th commit

--- script.js
+++ script.js
@@ -35,3 +35,9 @@ const person = {
     name: 'Alex',
     age: 25,
 };
+
+
+const serializedPerson = JSON.stringify(person);
+localStorage.setItem('alex', serializedPerson);
+
+console.log(JSON.parse(localStorage.getItem('alex')));
